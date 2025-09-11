d8e41f22bdcf081de22e97a7242fdf0859e11fdb
Melvin Shieh <melvin@Melvins-Air.lan>
2025-09-11 14:43:24 -0400

    added edit form download

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     flash,
     get_flashed_messages,
     redirect,
+    request,
     render_template,
     send_from_directory,
     session,
@@ -16,34 +17,65 @@ app = Flask(__name__)
 app.secret_key = 'secret1'
 
 root = os.path.abspath(os.path.dirname(__file__))
-data = os.path.join(root, 'cms', 'data')
-app.config['UPLOAD_FOLDER'] = data
+def get_data_path():
+    if app.config['TESTING']:
+        return os.path.join(root, 'tests', 'data')
+    else:
+        return os.path.join(root, 'cms', 'data')
 
 @app.route('/')
 def index():
+    data = get_data_path()
     files = os.listdir(data) 
     return render_template('index.html', files=files)
 
-@app.route("/files/<path:file_name>")
+@app.route("/<path:file_name>")
 def file_content(file_name):
+    data = get_data_path()
     file_path = os.path.join(data, file_name)
 
-    if file_name in os.listdir(data):
+    if os.path.isfile(file_path):
         if file_name.split('.')[-1] == 'md':
             with open(file_path, 'r') as file:
                 content = file.read()
-            return markdown(content)
+            return render_template('markdown.html', content=markdown(content))
         else:
             return send_from_directory(data, file_name)
     else:
         flash(f'{file_name} does not exist.', 'error')
         return redirect(url_for('index'))
 
-@app.route("/download/<path:file_name>")
+@app.route("/<path:file_name>/download")
 def download_file(file_name):
     return send_from_directory(
-        app.config['UPLOAD_FOLDER'], file_name, as_attachment=True
+        get_data_path(), 
+        file_name, 
+        as_attachment=True
     )
+
+@app.route("/<path:file_name>/edit")
+def edit_file(file_name):
+    data = get_data_path()
+    file_path = os.path.join(data, file_name)
+
+    if os.path.isfile(file_path):
+        with open(file_path, 'r') as file:
+            content = file.read()
+        return render_template('edit.html', file_name=file_name, content=content)
+    else:
+        flash(f'{file_name} does not exist.', 'error')
+        return redirect(url_for('index'))
+
+@app.route("/<path:file_name>", methods=["POST"])
+def submit_changes(file_name):
+    data = get_data_path()
+    file_path = os.path.join(data, file_name)
+    content = request.form['content']
+    with open(file_path, 'w') as file:
+        file.write(content)
+
+    flash(f'{file_name} has been edited.', 'success')
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
     if os.environ.get('FLASK_ENV') == 'production':
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,12 +1,27 @@
 import unittest
+import shutil
+import os
 from app import app
 
 class AppTest(unittest.TestCase):
     def setUp(self):
         app.config['TESTING'] = True
         self.client = app.test_client()
+        self.data_path = os.path.join(os.path.dirname(__file__), 'data')
+        os.makedirs(self.data_path, exist_ok=True)
 
+    def tearDown(self):
+        shutil.rmtree(self.data_path, ignore_errors=True)
+    
+    def create_document(self, name, content=""):
+        with open(os.path.join(self.data_path, name), 'w') as file:
+            file.write(content)
+    
     def test_index(self):
+        self.create_document("changes.txt")
+        self.create_document("about.md")
+        self.create_document("history.txt")
+
         response = self.client.get('/')
         print(response)
         self.assertEqual(response.status_code, 200)
@@ -17,14 +32,16 @@ class AppTest(unittest.TestCase):
         self.assertIn('history.txt', response.text)
     
     def test_document_view(self):
-        with self.client.get(f'/files/changes.txt') as response:
+        self.create_document("changes.txt", content='There are many changes.')
+
+        with self.client.get(f'/changes.txt') as response:
             self.assertEqual(response.status_code, 200)
             self.assertEqual(response.content_type, "text/plain; charset=utf-8")
             self.assertIn('There are many changes.', response.get_data(as_text=True))
     
     def test_document_not_found(self):
         # Attempt to access a non-existent file and verify a redirect occurs
-        with self.client.get('/files/nonexistant.ext') as response:
+        with self.client.get('/nonexistant.ext') as response:
             self.assertEqual(response.status_code, 302)
             self.assertEqual(response.headers.get('Location'), '/')
         
@@ -39,10 +56,32 @@ class AppTest(unittest.TestCase):
             self.assertNotIn('nonexistant.ext does not exist', response.get_data(as_text=True))
 
     def test_markdown_document_view(self):
-        response = self.client.get('/files/about.md')
+        self.create_document("about.md", content='# Python is...')
+
+        response = self.client.get('/about.md')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.content_type, "text/html; charset=utf-8")
         self.assertIn("<h1>Python is...</h1>", response.get_data(as_text=True))
+
+    def test_edit_file(self):
+        self.create_document("changes.txt")
+
+        response = self.client.get('/changes.txt/edit')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content_type, "text/html; charset=utf-8")
+        self.assertIn("<textarea", response.text)
+        self.assertIn('<button type="submit"', response.text)
+
+    def test_submit_changes(self):
+        with self.client.post('/changes.txt', data={'content': "There are many changes. New content."}) as response:
+            self.assertEqual(response.status_code, 302)
+
+        follow_response = self.client.get(response.headers['Location'])
+        self.assertIn("changes.txt has been edited", follow_response.get_data(as_text=True))
+
+        with self.client.get("/changes.txt") as content_response:
+            self.assertEqual(content_response.status_code, 200)
+            self.assertIn("New content.", content_response.get_data(as_text=True))
 
 if __name__ == "__main__":
     unittest.main()
